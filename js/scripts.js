51c8ee441e59f5cc783751cc03354c915cddea39
Narendra <75013042+nkrider7@users.noreply.github.com>
2023-07-26 23:26:23 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+const wtext = document.querySelector('#wtext');
+
+wtext.addEventListener('click',function(){
+
+    var canvas = document.createElement('canvas');
+    canvas.width = 500;
+    canvas.height = 250; 
+
+});
